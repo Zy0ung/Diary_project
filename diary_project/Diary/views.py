4fc02c89660edc42e1aa9328fe8d0318ae8915cf
Jiyoung <thdwldud8@gmail.com>
2021-07-14 00:21:26 +0900

    일기 쓰기 구현

--- diary_project/Diary/views.py
+++ diary_project/Diary/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from Diary.models import Diary
+from django.utils import timezone
 
 # Create your views here.
+def home(request):
+    diaries = Diary.objects.all()
+    return render(request, 'home.html', {'diaries':diaries})
+
+def create(request):
+    if request.method == 'POST':
+            new_diary = Diary()
+            new_diary.title = request.POST['title']
+            new_diary.body = request.POST['body']
+            new_diary.pub_date = timezone.now()
+            new_diary.weather = request.POST['weather']
+            new_diary.save()
+            return redirect('home')
+    else:
+        return render(request, 'new.html')
--- diary_project/diary_project/urls.py
+++ diary_project/diary_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Diary import views as D
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',D.home, name='home'),
+    path('diary/create', D.create ,name='create'),
 ]
